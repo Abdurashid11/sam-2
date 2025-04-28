dddeb5bb92e07ac5a836b6e6693228d20cfc229b
instagram.com <sanjar3370062@gmail.com>
2025-04-28 16:59:12 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleTheme() {
+    document.body.style.background = 'black'
+    document.body.style.transition = 'all 1s'
+    document.body.style.color = 'white'
+}
+
+function hel() {
+    document.body.style.background = 'white'
+    document.body.style.transition = 'all 1s'
+    document.body.style.color = 'black'
+}
+
+alert('Добро пожаловать на вторую странитсу сайта Pepsi.')
+
